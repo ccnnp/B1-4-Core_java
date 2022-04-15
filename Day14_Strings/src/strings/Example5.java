616606334df62fdc4000e7b081670dc7e1353e17
Home <Home@LAPTOP-QFF9CGK9>
2022-04-15 16:50:42 +0530

    First Commit

---
+++ Day14_Strings/src/strings/Example5.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Example5 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		String str1 = s.nextLine();
+		System.out.println(str1.length());
+		System.out.println(str1.charAt(1));
+		System.out.println(str1.toUpperCase());
+		System.out.println(str1.toLowerCase());
+		s.close();
+	}
+
+}
